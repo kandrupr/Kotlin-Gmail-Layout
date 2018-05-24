f355dbbdf610e85fdf0a658b609f30bb0e8faadf
pkkan <pk.kandru@gmail.com>
2018-05-24 13:21:29 -0400

    Initial Commit

---
+++ app/src/main/java/pr/kandru/gmailkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package pr.kandru.gmailkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
