507dedd3af69f81de33fd7dd4b8e263808c5f9ec
pkkan <pk.kandru@gmail.com>
2018-06-13 00:14:25 -0400

    Added icons. Floating Action Button, Navigation Drawer, and listeners.

--- app/src/main/java/pr/kandru/gmailkotlin/MainActivity.kt
+++ app/src/main/java/pr/kandru/gmailkotlin/MainActivity.kt
@@ -3,16 +3,28 @@ package pr.kandru.gmailkotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var toolbar: Toolbar;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
+        val fab = findViewById<android.support.design.widget.FloatingActionButton>(R.id.fab)
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setTitle("Primary")
+        toolbar.setNavigationIcon(R.drawable.baseline_menu_white_24);
+        toolbar.inflateMenu(R.menu.menu_main);
         setSupportActionBar(toolbar)
-        toolbar.setTitle("Gmail Kotlin")
+        toolbar.setNavigationOnClickListener(View.OnClickListener {
+            // WORKS
+            Log.d("Click", "Navigation Drawer");
+        });
+
+        fab.setOnClickListener(View.OnClickListener {
+           Log.d("Click", "FAB")
+        });
     }
 }
